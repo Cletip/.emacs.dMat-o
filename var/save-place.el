5ddfa828a25405584d23283e9375b3e2e0ea0bf9
Cletip <clement020302@gmail.com>
2023-03-30 13:58:37 +0200

    moddif mineur et précision readme

---
+++ var/save-place.el
@@ -0,0 +1,4 @@
+;;; -*- coding: utf-8; mode: lisp-data -*-
+(("//home/utilisateur/.emacs.dMateo/.emacs.d/straight/build/orderless/orderless-autoloads.el" . 1692)
+ ("//home/utilisateur/.emacs.dMateo/.emacs.d/straight/build/marginalia/marginalia-autoloads.el" . 1435)
+ ("//home/utilisateur/.emacs.dMateo/.emacs.d/straight/build/vertico/vertico-autoloads.el" . 1250))
